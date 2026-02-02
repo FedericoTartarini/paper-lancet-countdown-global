82d114537bb96385313a5a1b404fca08202ff61a
Federico Tartarini <federicotartarini@gmail.com>
2026-02-02 13:51:45 +1100

    feat: update sync_with_rsync function to accept source and destination parameters

--- python_code/copy_files_hpc.py
+++ python_code/copy_files_hpc.py
@@ -1,15 +1,13 @@
 import subprocess
 
 
-def sync_with_rsync():
+def sync_with_rsync(source: str = None, dest: str = None):
     # Use 'sshpass' if you want to automate the password,
     # otherwise it will prompt you in the terminal.
     # On Mac: brew install sshpass
 
     # If you use a trailing slash on the source (daily/): It copies the contents of the folder into the destination.
     # If you do NOT use a trailing slash (daily): It copies the folder itself into the destination.
-    source = "/Users/ftar3919/Library/CloudStorage/OneDrive-TheUniversityofSydney(Staff)/data/lancet/countdown-europe/data/2027/GRIDDED"
-    dest = "ft8695@gadi-dm.nci.org.au:/scratch/mn51/ft8695/countdown-europe/data/2027/"
 
     # Command explanation:
     # -a: Archive mode (keeps permissions/dates)
